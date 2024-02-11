2cd75369dc9f3ca8401cdd5777b9ba80c6134d7b
Ahlam Alsakkaf <157818610+Ahlam2024@users.noreply.github.com>
2024-02-11 06:50:36 -0800

    Add files via upload

---
+++ JS Basic/index.js
@@ -0,0 +1,64 @@
+//__JS Basic__
+//1 
+//for ( let i = 0 ; i <= 13; i++)
+//{
+//    console.log(i);
+//}
+
+//2
+//for ( let i = 1 ; i <= 15; i +=2)
+//{
+//    console.log(i);
+//}
+
+//3
+//let sum = 0;
+//for ( let i = 1 ; i <= 20; i++)
+//{
+//    sum+=i,
+//    console.log("the number is "+ i +"the sum is"+ sum);
+//}
+
+//4
+//let x =[1, 7, 9, 23, "ahlam"];
+//
+//for (let index = 0; index < x.length; index++){
+//    console.log(x[index]);
+//}
+
+//5
+//let xx = [4,-8,-6]
+//let maxNum = xx[0];
+//
+//for (let index= 0; index<xx.length; index++)
+//{
+//    if(x[index] > maxNum) {
+//        maxNum = xx[index];
+//    }
+//}
+//console.log(maxNum);
+
+//6
+//let xxx = [3, 6, 8];
+
+//let averageNum = xxx[0]/ xxx.length;
+
+//console.log(averageNum);
+
+//7 
+//const x = [4, -3, 7];
+//for (let i = 0; i < x.length; i++) {
+//    if (x[i] < 0) {
+//        X[i] = 0;
+//    }
+//}
+//console.log(X);
+
+//8 
+//let x=[2, 4, 6, -7, -4, -9];
+//for (let i = 0; i<=x.length; i++){
+//    if(x[i] < 0) {
+//        x[i]= "turning"
+//    }
+//}
+//  console.log(x);
